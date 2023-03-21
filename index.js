8d130c01d2266a61403b0af4ec5b50bb86c16810
jessicarocham <jessicarocham18@hotmail.com>
2023-03-21 09:01:56 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+let names = [`Charlie`, `Samip`, `Ali`];
+function writeCards(names) {
+  let cards = [];
+  for (let i = 0; i < names.length; i++) {
+    cards[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+  }
+  return cards;
+}
+writeCards(names);
+
+function countDown() {
+  let countDown = 10;
+  while (countDown >= 0) {
+    console.log(countDown--);
+  }
+}
